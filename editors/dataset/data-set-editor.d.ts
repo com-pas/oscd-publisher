7a37f1df65240c3af673816e41ae2cd625a44b8e
noraeb <n.blomaard@gmail.com>
2026-04-09 07:10:55 +0000

    Deploying to deploy from @ com-pas/oscd-publisher@5eb10e92b55d4d07d909b46aa2ccd0a2afae1889 🚀

--- editors/dataset/data-set-editor.d.ts
+++ editors/dataset/data-set-editor.d.ts
@@ -1,13 +1,25 @@
 import { LitElement, TemplateResult } from 'lit';
 import { ActionList } from '@openenergytools/filterable-lists/dist/ActionList.js';
 import { MdOutlinedButton } from '@scopedelement/material-web/button/MdOutlinedButton.js';
+import { MdDialog } from '@scopedelement/material-web/dialog/MdDialog.js';
+import { MdIcon } from '@scopedelement/material-web/icon/MdIcon.js';
+import { MdTextButton } from '@scopedelement/material-web/button/MdTextButton.js';
+import { MdRadio } from '@scopedelement/material-web/radio/radio.js';
+import { MdList } from '@scopedelement/material-web/list/MdList.js';
+import { MdListItem } from '@scopedelement/material-web/list/MdListItem.js';
 import { DataSetElementEditor } from './data-set-element-editor.js';
 declare const DataSetEditor_base: typeof LitElement & import("@open-wc/scoped-elements/lit-element.js").ScopedElementsHostConstructor;
 export declare class DataSetEditor extends DataSetEditor_base {
     static scopedElements: {
         'action-list': typeof ActionList;
+        'md-text-button': typeof MdTextButton;
         'data-set-element-editor': typeof DataSetElementEditor;
         'md-outlined-button': typeof MdOutlinedButton;
+        'md-dialog': typeof MdDialog;
+        'md-icon': typeof MdIcon;
+        'md-radio': typeof MdRadio;
+        'md-list': typeof MdList;
+        'md-list-item': typeof MdListItem;
     };
     /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
     doc: XMLDocument;
@@ -15,9 +27,13 @@ export declare class DataSetEditor extends DataSetEditor_base {
     editCount: number;
     searchValue: string;
     selectedDataSet?: Element;
+    lDevices: Element[];
+    selectedLDevice: Element | null;
+    selectedIed: Element | null;
     selectionList: ActionList;
     selectDataSetButton: MdOutlinedButton;
     dataSetElementEditor: DataSetElementEditor;
+    lDeviceSelectDialog: MdDialog;
     /** Resets selected DataSet, if not existing in new doc
     update(props: Map<string | number | symbol, unknown>): void {
       if (props.has('doc') && this.selectedDataSet) {
@@ -35,7 +51,11 @@ export declare class DataSetEditor extends DataSetEditor_base {
     updated(changedProps: Map<string | number | symbol, unknown>): void;
     private renderElementEditorContainer;
     private renderSelectionList;
+    private createDataSet;
     private renderToggleButton;
+    private renderLDeviceSelectDialog;
+    private selectLDevice;
+    private handleLDeviceSelect;
     render(): TemplateResult;
     static styles: import("lit").CSSResult;
 }
